f0838b3e3579f2f2d75f1b82dc1c01cd8e503e13
Csizmadia Zsolt (CC-AD/EYC1-Bp) <Zsolt.Csizmadia@hu.bosch.com>
2019-11-27 12:01:55 +0100

    [python_practice] multithreading example fixed.

--- extra/01_multithreading.py
+++ extra/01_multithreading.py
@@ -3,21 +3,34 @@ import _thread
 import time
 from random import randint
 
-LIST_MAX_VALUE = 2
+global_a = 0
+global_b = 0
 
 def increase(thread_name):
-  a = 0
-  b = 0
+  global global_a
+  global global_b
 
-  while a < 10:
-    time.sleep(randint(0,LIST_MAX_VALUE))
+  while (global_a < 10 and global_b < 10):
+    time.sleep(randint(1,20))
 
     if thread_name == "A":
-      a = a + 1
-    else:
-      b = b + 1
+      global_a += 1
 
-    generate_output(a,b)
+    if thread_name == "B":
+      global_b += 1
+	  
+def output_scheduler():
+  global global_a
+  global global_b
+
+  while (global_a < 10 and global_b < 10):
+    time.sleep(0.1)
+    generate_output(global_a,global_b)
+
+  if(global_a > global_b):
+    print("\nThe winner is A! (" + str(global_a) + "-" + str(global_b) + ")")
+  else:
+    print("\nThe winner is B! (" + str(global_a) + "-" + str(global_b) + ")")
 
 def generate_output(number_of_As, number_of_Bs):
   print("\rA: [", end="")
@@ -28,12 +41,11 @@ def generate_output(number_of_As, number_of_Bs):
   print(" "*int(10-number_of_Bs),end="")
   print("]",end="")
   
-generate_output(0,0)
-  
 try:
-   _thread.start_new_thread( increase, ("A", ) )
+   _thread.start_new_thread(increase, ("A", ))
+   _thread.start_new_thread(increase, ("B", ))
+   _thread.start_new_thread(output_scheduler, ())
 except:
    print("Error: unable to start thread")
    
-while 1:
-  pass
+input()
