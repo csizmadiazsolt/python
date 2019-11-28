2db919081f2c844262e9c1fc09a9f8ee74774e3c
Csizmadia Zsolt (CC-AD/EYC1-Bp) <Zsolt.Csizmadia@hu.bosch.com>
2019-11-28 15:14:53 +0100

    [python_practice] Guessing game added.

---
+++ 09_guessing_game_one/09_guessing_game_one.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from random import randint
+
+generated_number = randint(1,9)
+
+print("Use 'exit' if you want to exit the application!\n")
+
+while True:
+  user_command = input("Guess a number between 1-9: ")
+  
+  if(user_command.isnumeric()):
+    if(int(user_command) > generated_number):
+      print("Too high!")
+    elif(int(user_command) < generated_number):
+      print("Too low!")
+    elif(int(user_command) == generated_number):
+      print("Exactly right!")
+      break
+  else:
+    if(user_command == "exit"):
+      break
+    else:
+      continue
