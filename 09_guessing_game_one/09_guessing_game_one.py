b6bc8f7ee2d3c5d8f2a600b8fe51a6578428ac5c
Csizmadia Zsolt (CC-AD/EYC1-Bp) <Zsolt.Csizmadia@hu.bosch.com>
2019-11-28 15:19:42 +0100

    [python_practice] Guessing game extra

--- 09_guessing_game_one/09_guessing_game_one.py
+++ 09_guessing_game_one/09_guessing_game_one.py
@@ -2,11 +2,13 @@
 from random import randint
 
 generated_number = randint(1,9)
+number_of_attempts = 0
 
 print("Use 'exit' if you want to exit the application!\n")
 
 while True:
   user_command = input("Guess a number between 1-9: ")
+  number_of_attempts += 1
   
   if(user_command.isnumeric()):
     if(int(user_command) > generated_number):
@@ -21,3 +23,5 @@ while True:
       break
     else:
       continue
+	  
+print("\nNumber of attempts: " + str(number_of_attempts))
