fdcc754fffed6da194dea2bebe33c097cd5eb1d6
Csizmadia Zsolt (CC-AD/EYC1-Bp) <Zsolt.Csizmadia@hu.bosch.com>
2019-12-02 09:42:31 +0100

    [python_practice] 16 password generator added.

---
+++ 16_password_generator/16_password_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from random import randint
+
+def generate_password(length):
+  password = ""
+
+  for i in range(0,length):
+    pass_char = randint(33,126)
+    password += chr(pass_char)
+	
+  return password
+
+print(generate_password(15))
