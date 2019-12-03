8aed27856e2e74f0f0002f3e8dcf01a900ceeec2
Csizmadia Zsolt (CC-AD/EYC1-Bp) <Zsolt.Csizmadia@hu.bosch.com>
2019-12-03 14:33:30 +0100

    [python_practice] write to a file exercise.

--- 21_write_to_a_file/21_write_to_a_file.py
+++ 21_write_to_a_file/21_write_to_a_file.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+def save_to_file(filename, content):
+  open_file = open(filename, 'w')
+  open_file.write(content)
+  open_file.close()
+
+def get_input(message):
+  input_message = str(input(message))
+  return input_message
+  
+filename = get_input("Enter the filename: ")
+content = get_input("Enter the content: ")
+
+save_to_file(filename, content)
