df70855eca18883be1eacfaab57284ec3e84139d
Csizmadia Zsolt (CC-AD/EYC1-Bp) <Zsolt.Csizmadia@hu.bosch.com>
2019-12-05 12:56:57 +0100

    [python_practice] Game board drawer extension!

--- 24_draw_a_game_board/24_draw_a_game_board.py
+++ 24_draw_a_game_board/24_draw_a_game_board.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
 
 VERT_SEGMENT = " ---"
-HORI_SEGMENT = "|   "
+HORI_SEGMENT1 = "| "
+HORI_SEGMENT2 = " "
 HORI_END_SEGMENT = "|"
 
-def draw_board(size_x, size_y):
-  for _ in range(0,size_y):
+EMPTY = " "
+PLAYER1 = "O"
+PLAYER2 = "X"
+
+def draw_board(size_x, size_y, game_matrix):
+  for i in range(0,size_y):
     for _ in range(0,size_x):
       print(VERT_SEGMENT,end="")
     print("\n",end="")
-    for _ in range(0,size_x):
-      print(HORI_SEGMENT,end="")
+    for k in range(0,size_x):
+      print(HORI_SEGMENT1,end="")
+	  
+      if game_matrix[i][k] == 0:
+        print(EMPTY,end="")
+      elif game_matrix[i][k] == 1:
+        print(PLAYER1,end="")
+      elif game_matrix[i][k] == 2:
+        print(PLAYER2,end="")
+
+      print(HORI_SEGMENT2,end="")
     print(HORI_END_SEGMENT)
   for _ in range(0,size_x):
     print(VERT_SEGMENT,end="")
 
-draw_board(7,7)
+game = [[1, 0, 2],
+	    [0, 1, 0],
+	    [0, 0, 2]]
+
+draw_board(3, 3, game)
