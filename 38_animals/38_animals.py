a1f73160c0c18cff1990be1247cdc3e2393ff843
Csizmadia Zsolt (CC-AD/EYC1-Bp) <Zsolt.Csizmadia@hu.bosch.com>
2019-12-11 14:06:55 +0100

    [python_practice] Animals minor change.

--- 38_animals/38_animals.py
+++ 38_animals/38_animals.py
@@ -1,12 +1,14 @@
 # !/usr/bin/python3
 
 class Animal:
+  numberOfAnimals = 0
   def __init__(self, name):
-    print("A new animal created!")
+    Animal.numberOfAnimals += 1
+    print("Animal {} created.".format(Animal.numberOfAnimals))
     self.name = name
 
-  def getName(self):
-    return self.name
+  def getInfo(self):
+    print("{} is the name of the animal.".format(self.name))
 
 class Mammal(Animal):
   def __init__(self, name, numberOfLegs = 4):
@@ -25,10 +27,12 @@ class Bird(Animal):
   def getInfo(self):
     print("{} has {} legs and {} wings.".format(self.name, self.numberOfLegs, self.numberOfWings))
 
+animal = Animal("Generopida")
 dog = Mammal("Rex")
 whale = Mammal("Mr. Whale", 0)
 blackbird = Bird("Blacky")
 
+animal.getInfo()
 dog.getInfo()
 whale.getInfo()
 blackbird.getInfo()
